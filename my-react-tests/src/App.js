f3d9484443d9d64c0902b372a6b603bb18214245
ijromerob <ijromerob@gmail.com>
2024-07-01 15:34:38 -0600

    new changes

--- my-react-tests/src/App.js
+++ my-react-tests/src/App.js
@@ -1,25 +1,56 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import PropTypes from 'prop-types';
 import logo from './logo.svg';
 import './App.css';
+
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Mycomponent user="Ivan" />
       </header>
     </div>
   );
 }
+
+
+
+class Mycomponent extends Component {
+  constructor(props) {
+    super(props);
+  }
+  state = {}
+  render() {
+    return (<div><h1>hello world {this.props.user}</h1>
+      {/* passing an array as prop */}
+      <PassPP tasks={["walk the dog", "workout"]} />
+      <Anothercomponent /></div>
+    );
+  }
+}
+
+class PassPP extends Component {
+  constructor(props) {
+    super(props);
+  }
+  state = {}
+  render() {
+    // passing an array and having a method in here 
+    return (<h2>{this.props.tasks.join(", ")}</h2>);
+  }
+}
+
+// making sure that the type of property is correct
+PassPP.propTypes = { tasks: PropTypes.array.isRequired }
+
+function Anothercomponent(props) {
+  return (<p>{props.user}</p>);
+}
+
+Anothercomponent.defaultProps = { user: "Andrada" }
+
 
 export default App;

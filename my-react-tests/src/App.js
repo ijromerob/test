e101b03c5d5f03d9d5e9dc915f90ca3af45c1a21
ijromerob <ijromerob@gmail.com>
2024-07-02 12:01:17 -0600

    changes

--- my-react-tests/src/App.js
+++ my-react-tests/src/App.js
@@ -22,15 +22,48 @@ class Mycomponent extends Component {
   constructor(props) {
     super(props);
   }
-  state = {}
+  state = {
+    name: 'Web development'
+  }
+
   render() {
+    const name = this.state.name;
+
     return (<div><h1>hello world {this.props.user}</h1>
       {/* passing an array as prop */}
       <PassPP tasks={["walk the dog", "workout"]} />
-      <Anothercomponent /></div>
+      <Anothercomponent />
+      <div>{this.state.name}</div>
+      <ButtonTest />
+    </div>
     );
   }
 }
+
+class ButtonTest extends Component {
+  constructor(props) {
+    super(props);
+    //put here the this thing
+    this.clickHandler = this.clickHandler.bind(this);
+  }
+  state = { name: "initial State" }
+  clickHandler() {
+    this.setState({
+      name: "secondary State"
+    })
+  }
+  render() {
+    return (
+      <div>
+        <button onClick={this.clickHandler}>
+          Click here
+        </button>
+        <h1>{this.state.name}</h1>
+      </div>
+    );
+  }
+}
+
 
 class PassPP extends Component {
   constructor(props) {
